4f5a2c4b57acc82598d5497e12dac157d0dc5815
Guo An Wang <you@example.com>
2025-09-19 12:09:21 -0700

    second commit

--- Lab 12: STD::array/main.cpp
+++ Lab 12: STD::array/main.cpp
@@ -3,7 +3,10 @@
 //  COMSC-210 | Lab 12: Lab 12: STD::array | Guo An Wang
 //  IDE used: Xcode
 //  Created by Guo An Wang on 9/19/25.
-//
+//  Program use:
+//  This program takes in the txt file weather which is the daily temperature
+//  high for Lafayette,CA for the next 30 days. It also uses various array member
+//  functions to demonstrate knowledge
 
 #include <iostream>
 #include <fstream>
@@ -11,9 +14,21 @@
 #include <numeric>
 #include <array>
 using namespace std;
+const int SIZE = 30; //size of data file
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    array <int,SIZE> weather;
+    ifstream inputFile("weather");
+    if (!inputFile) {
+        cout << "Error: Could not access input file" << endl;
+        return 1;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        inputFile >> weather[i];
+    }
+    inputFile.close();
+    
+    cout << "1. Size: " << weather.size() << endl;
+    
     return 0;
 }
